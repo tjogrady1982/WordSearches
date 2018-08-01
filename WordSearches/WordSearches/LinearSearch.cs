648440586e558bcca079d902c998a8a1f0bd01fc
Tim O'Grady <Tim.OGrady@anthonynolan.org>
2018-08-01 20:42:15 +0100

    added linear search

---
+++ WordSearches/WordSearches/LinearSearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSearches
+{
+    public class LinearSearch
+    {
+        public string FindWord(string word)
+        {
+            foreach (var item in WordList.wordList)
+            {
+                if (item == word)
+                {
+                    
+                    break;
+                }
+
+              
+            }
+            return ("Word Found");
+        }
+    }
+}
